61559169ac1f19c25b68512df3bcc58cfdfe13de
Max <max.mraz.11@gmail.com>
2018-09-17 09:48:18 -0400

    Oops, I mis committed like twenty versions.

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# Hey, it's a tomagachi!
+# How to care for your robot:
+
+##You robot has 5 stats:
+
+Battery - you want to keep your robot's battery charged
+Happiness - you want your robot to love you
+Health - you don't want to catch a computer virus!
+Boredom - with this much processing power, your bot needs to be using it
+Self Awareness - !?
+
+##Statuses
+Virus
+Hacking
+On A Walk?
+
+
+##What do you want to do with your robot?
+Do Nothing (happ --, health -, bored ++)
+
+Charge Robot (batt +++, happ -, bored ++, health +)
+
+Feed Robot (batt+, happ (depends on food), health (depends on food))
+	Foods:
+	-nuts and bolts
+	-broken appliance
+	-oreos
+	-porridge
+
+Garden together (batt -, happ ++, bored -)
+
+Watch TV (batt -, happ +, health -, bored 
+
+
+
+
+Go on a walk (batt --, happ +, health +, bored --, sa +,) (you're on a walk)
+	-stop at a park (batt ---, happ ++, bored ---, sa+)
+
+
+Dig a copper mine (batt --, happ -, health -, bored +) (you found a bunch of copper ore)
+	-Process copper ore (batt -, happ +) (you made some copper wires)
+		-upgrade Robot (batt -, health++, sa+)
+
--- src/test/java/virtual_pet/VirtualPetTest.java
+++ src/test/java/virtual_pet/VirtualPetTest.java
@@ -1,5 +1,154 @@
 package virtual_pet;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class VirtualPetTest {
+
+	@Test
+	public void shouldHaveDefaultValues() {
+		VirtualPet underTest = new VirtualPet("");
+		int sA = underTest.getSelfAwareness();
+		int batt = underTest.getBattery();
+		int happ = underTest.getHappiness();
+		int health = underTest.getHealth();
+		int bord = underTest.getBoredom();
+		int hack = underTest.getHacking();
+		boolean sick = underTest.getVirus();
+		Assert.assertEquals(0, sA);
+		Assert.assertEquals(100, batt);
+		Assert.assertEquals(0, happ);
+		Assert.assertEquals(100, health);
+		Assert.assertEquals(0, bord);
+		Assert.assertEquals(0, hack);
+		Assert.assertEquals(false, sick);
+	}
+
+	@Test
+	public void shouldHaveFaveFood() {
+		VirtualPet underTest = new VirtualPet("");
+		Assert.assertNotEquals(underTest.getFaveFood(), null);
+	}
+
+//	//I'm not how to test a method that just prints to the console?
+//	//What am I asserting?
+//	@Test
+//	public void shouldSurviveSingularity() {
+//		VirtualPet underTest = new VirtualPet();
+//		underTest.setHappiness(0);
+//		underTest.setSelfAwareness(120);
+//		underTest.tick();
+//	}
+//	@Test
+//	public void shouldCallFBI() {
+//		VirtualPet underTest = new VirtualPet();
+//		underTest.setHacking(120);
+//		underTest.tick();
+//	}
+//	@Test
+//	public void shouldShowStats() {
+//		VirtualPet underTest = new VirtualPet("Choppy");
+//		underTest.displayStats();
+//		underTest.setHacking(20);
+//		underTest.displayStats();
+//	}
+
+	@Test
+	public void shouldHaveErrorAffectStats() {
+		VirtualPet underTest = new VirtualPet("");
+		int batt = underTest.getBattery();
+		int happ = underTest.getHappiness();
+		int selfA = underTest.getSelfAwareness();
+		int hack = underTest.getHacking();
+		int errMsg = underTest.haveError();
+		if (errMsg < 3) {
+			Assert.assertNotEquals(batt, underTest.getBattery());
+		} else if (errMsg < 6) {
+			Assert.assertNotEquals(happ, underTest.getHappiness());
+		} else if (errMsg < 8) {
+			Assert.assertNotEquals(selfA, underTest.getSelfAwareness());
+		} else if (errMsg == 8) {
+			Assert.assertNotEquals(hack, underTest.getHacking());
+		}
+	}
+
+	@Test
+	public void shouldSkipTurnOrWaitWhateverHappy() {
+		VirtualPet underTest = new VirtualPet("");
+		int happ = underTest.getHappiness();
+		underTest.doNothing();
+		Assert.assertEquals(happ - 10, underTest.getHappiness());
+	}
+
+	@Test
+	public void shouldSkipTurnOrWaitWhateverHealth() {
+		VirtualPet underTest = new VirtualPet("");
+		int happ = underTest.getHealth();
+		underTest.doNothing();
+		Assert.assertEquals(happ - 5, underTest.getHealth());
+	}
+
+	@Test
+	public void shouldSkipTurnOrWaitWhateverBord() {
+		VirtualPet underTest = new VirtualPet("");
+		int bor = underTest.getBoredom();
+		underTest.doNothing();
+		Assert.assertEquals(bor + 10, underTest.getBoredom());
+	}
+
+	@Test
+	public void shouldFeedRobotOreosWhenFave() {
+		VirtualPet underTest = new VirtualPet("");
+		underTest.setFaveFood(2);
+		int hel = underTest.getHealth();
+		int happ = underTest.getHappiness();
+		int batt = underTest.getBattery();
+		int sum = hel + happ + batt;
+		underTest.feedRobot(2);
+		hel = underTest.getHealth();
+		happ = underTest.getHappiness();
+		batt = underTest.getBattery();
+		int sum2 = hel + happ + batt;
+		Assert.assertEquals(sum + 40, sum2);
+	}
+	
+	@Test
+	public void shouldFeedRobotOreosWhenNotFave() {
+		VirtualPet underTest = new VirtualPet("");
+		underTest.setFaveFood(3);
+		int hel = underTest.getHealth();
+		int happ = underTest.getHappiness();
+		int batt = underTest.getBattery();
+		int sum = hel + happ + batt;
+		underTest.feedRobot(2);
+		hel = underTest.getHealth();
+		happ = underTest.getHappiness();
+		batt = underTest.getBattery();
+		int sum2 = hel + happ + batt;
+		Assert.assertEquals(sum + 20, sum2);
+	}
+	
+	@Test
+	public void shouldGardenBatt(){
+		VirtualPet underTest = new VirtualPet("");
+		int batt = underTest.getBattery();
+		underTest.garden();
+		Assert.assertEquals(batt-15, underTest.getBattery());
+	}
+	@Test
+	public void shouldGardenHapp(){
+		VirtualPet underTest = new VirtualPet("");
+		int batt = underTest.getHappiness();
+		underTest.garden();
+		Assert.assertEquals(batt+15, underTest.getHappiness());
+	}
+	@Test
+	public void shouldGardenBore(){
+		VirtualPet underTest = new VirtualPet("");
+		underTest.setBoredom(50);
+		int batt = underTest.getBoredom();
+		underTest.garden();
+		Assert.assertEquals(batt-15, underTest.getBoredom());
+	}
 
 }
